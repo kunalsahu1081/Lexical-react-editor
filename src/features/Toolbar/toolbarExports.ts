5ffb1746dc55ef7abe85c1b4d4805829631fe5d3
kunal <kunalsahu1081@gmail.com>
2025-08-27 02:02:14 +0530

    Fixing Exports

--- src/features/Toolbar/toolbarExports.ts
+++ src/features/Toolbar/toolbarExports.ts
@@ -1,25 +1,23 @@
-
-
 export {Bold, Italic, StrikeThrough, Underline} from './sections/commonTextControls';
 
 export {AlignLeft, AlignRight, AlignCenter, AlignJustify} from './sections/paragraphStyling';
 
-export * as AddChecklist from './sections/addChecklist';
+export { default as AddChecklist } from './sections/addChecklist';
 
-export * as AddIndent from './sections/addIndent';
+export { default as AddIndent } from './sections/addIndent';
 
-export * as RemoveIndent from './sections/removeIndent';
+export { default as RemoveIndent } from './sections/removeIndent';
 
-export * as AddOrderedList from './sections/addUnorderedList';
+export { default as AddOrderedList } from './sections/addUnorderedList';
 
-export * as FontPicker from './sections/fontPicker';
+export { default as FontPicker } from './sections/fontPicker';
 
-export * as FontSize from './sections/fontSize';
+export { default as FontSize } from './sections/fontSize';
 
-export * as HighlightText from './sections/highlightText';
+export { default as HighlightText } from './sections/highlightText';
 
-export * as LineSpacing from './sections/lineSpacing';
+export { default as LineSpacing } from './sections/lineSpacing';
 
-export * as ParagraphSpacing from './sections/paragraphSpacing';
+export { default as ParagraphSpacing } from './sections/paragraphSpacing';
 
-export * as TextColor from './sections/textColor';
+export { default as TextColor } from './sections/textColor';
